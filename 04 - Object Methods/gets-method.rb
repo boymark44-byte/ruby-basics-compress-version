ff1cb4df5910f91e1e4baa79f628ed57c16dc69f
Mark Glenn Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-12-10 20:23:52 +0800

    practice-coding: Added a file that contains the code for gets method since it can only be run through terminal rather than code runner.

---
+++ 04 - Object Methods/gets-method.rb
@@ -0,0 +1,12 @@
+# The gets method in Ruby
+puts "\nThe gets method in Ruby: "
+
+print "Hi, what's your name? "
+name = gets.chomp
+
+puts "Your name is #{name}? Nice to meet you!"
+
+print "Great, what's is your age? "
+age = gets.chomp
+
+puts "Your name is #{name} and you are #{age} years old. Awesome!"
