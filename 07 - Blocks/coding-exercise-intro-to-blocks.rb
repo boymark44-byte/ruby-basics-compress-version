77fd6352d5fba4fabd4f7cfba9eed9a334e924b7
Mark Glenn Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-12-11 18:08:35 +0800

    practice-coding: Committing the intro to blocks exercise.

---
+++ 07 - Blocks/coding-exercise-intro-to-blocks.rb
@@ -0,0 +1,18 @@
+def print_five_times
+  5.times { print "Hello" }
+end
+
+print_five_times
+
+
+puts
+
+
+def money_printer(num)
+  num.times { print "Money" }
+  puts
+end
+
+money_printer(3)
+money_printer(5)
+money_printer(0)
