3711f992626ec190917fdebc3019d3b0ca5750e3
Mark Glenn Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2023-12-15 16:58:12 +0800

    practice=coding: Committing the exercise for Section 10 which is to create an array.

---
+++ 12 - Arrays I/coding-exercise-creating-arrays.rb
@@ -0,0 +1,26 @@
+# Declare an empty_array method that returns an empty array
+def empty_array
+  []
+end
+
+p empty_array # => []
+
+
+puts
+# Declare a three_colors method that returns an array with
+# the strings of "red", "green", and "blue" (in that order)
+def three_colors
+  three_colors = ["red", "green", "blue"]
+end
+
+p three_colors # => ["red", "green", "blue"]
+
+
+puts
+# Declare a five_favorite_numbers method that returns an
+# array of 5 integers. The integers are up to you.
+def five_favorite_numbers
+  five_integers = [1, 2, 3, 4, 5]
+end
+
+p five_favorite_numbers # => [1, 2, 3, 4, 5]
